ff65d7452aeda8c2fb4788fcdb75847d751d11ff
marcel <marcel.soare@gmail.com>
2018-07-26 23:51:43 +0300

    add more debug stuff

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
 const express = require("express");
+//debug staff
 const chalk = require("chalk");
+const debug = require('debug')('app');
+const morgan = require('morgan');
 
 const app = express();
 const portNumber = 3000;
+
+app.use(morgan('tiny'));
 
 app.get('/', function(req,res){
     res.send('sample app');
 });
 
-
 app.listen(portNumber, function(){
-    console.log(`start server on port ${chalk.green(portNumber)}`);
+    debug(`start server on port ${chalk.green(portNumber)}`);
 });
