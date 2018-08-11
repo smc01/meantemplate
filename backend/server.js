85a1cf0df31229e6bcb8e3ac186d57a669cc087b
smc <marcel.soare@gmail.com>
2018-08-11 17:38:40 +0300

    add record

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,49 @@ const morgan = require('morgan');
 const app = express();
 const portNumber = process.env.port || 3000;
 
+const sqlite3 = require('sqlite3').verbose();
+
+
 app.use(morgan('tiny'));
 
 app.get('/', function(req,res){
     res.send('sample app');
 });
+app.get('/books', function (req, res) {
+    // open the database
+let db = new sqlite3.Database('./db/PersonalBookStore.db', sqlite3.OPEN_READWRITE, (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Connected to the chinook database.');
+  });
+   
+const sql = 'select id, title from  Book';
 
+  db.all(sql, [], (err, rows)=>{
+
+    if(err){
+        console.log('error on getting data');
+    }
+
+    rows.forEach((row)=>{
+        console.log(row.title);
+    });
+
+    res.json(rows);
+
+  });
+   
+  db.close((err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('Close the database connection.');
+  });
+  })
 app.listen(portNumber, function(){
     debug(`start server on port ${chalk.green(portNumber)}`);
 });
+
+
+
